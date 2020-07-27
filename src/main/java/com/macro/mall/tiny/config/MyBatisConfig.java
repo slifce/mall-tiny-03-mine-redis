37d147feeb9855be5768597443743c7875a921d1
slifce <734507707@qq.com>
2020-07-27 17:20:43 +0800

    jwt step 1

--- src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
+++ src/main/java/com/macro/mall/tiny/config/MyBatisConfig.java
@@ -7,6 +7,6 @@ import org.springframework.context.annotation.Configuration;
  * MyBatis配置类
  */
 @Configuration
-@MapperScan("com.macro.mall.tiny.mbg.mapper")
+@MapperScan({"com.macro.mall.tiny.mbg.mapper","com.macro.mall.tiny.dao"})
 public class MyBatisConfig {
 }
